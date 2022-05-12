8f1c5257a4354ea522ec07dfce4bab6311ca5dab
wangyb <codowang@163.com>
2022-05-12 14:55:01 +0800

    尝试把所有皮肤样式整合到当前项目中

--- src/main.js
+++ src/main.js
@@ -1,38 +1,39 @@
 // 第三方插件
-import store from './store'
-import router from './router/tab_router'
-import routes from './assets/data/routes'
+import store from './store';
+import router from './router/tab_router';
+import routes from './assets/data/routes';
 
 // 入口组件
-import App from './App.vue'
+import App from './App.vue';
 
 // 插件
-import './plugins/element'
-import './plugins/http'
+import './plugins/element';
+import './plugins/http';
 
 // 引入样式
-import './assets/css/index.less'
+// import './assets/css/index.less';
+import './assets/css/dark.less';
 
 var app = new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
 
 // 创建一个替换router实例的逻辑
-app.registRouter = function (router) {
-  if (!router) return
-  this._router = router
-  this._router.init(this)
-  Vue.util.defineReactive(this, '_route', this._router.history.current)
-}
+app.registRouter = function(router) {
+  if (!router) return;
+  this._router = router;
+  this._router.init(this);
+  Vue.util.defineReactive(this, '_route', this._router.history.current);
+};
 
 // 保存app，以便其他地方使用
-store.commit('initApp', app)
+store.commit('initApp', app);
 
 // 异步加载routes
 setTimeout(() => {
-  store.commit('Route/setRoutes', routes)
+  store.commit('Route/setRoutes', routes);
 }, 300);
 
-export default app
+export default app;
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,19 +2,20 @@
   <el-container class="home-conatiner fr-full">
     <el-header class="home-header padding__empty">
       <el-container class="fr-full"
-                    direction="horizontal">
+        direction="horizontal">
         <el-aside class="home-header-title"
-                  :width="leftAsideWidth">
+          :width="leftAsideWidth">
           <img class="home-header-title__logo"
-               src="/static/img/logo.png" />
+            src="/static/img/logo.png" />
           <span>后端管理</span>
         </el-aside>
         <el-main class="padding__small">
-          全文检索 / 常用功能 / 快捷菜单 / <span style="cursor: pointer;" @click="loadRoute">加载路由</span>
+          全文检索 / 常用功能 / 快捷菜单 / <span style="cursor: pointer;"
+            @click="loadRoute">加载路由</span>
         </el-main>
         <el-aside class="padding__small"
-                  :width="rightAsideWidth"
-                  style="text-align: right;">
+          :width="rightAsideWidth"
+          style="text-align: right;">
           个性化配置 / 用户个人信息
         </el-aside>
       </el-container>
@@ -22,9 +23,10 @@
     <el-main class="padding__empty">
       <el-container class="fr-full">
         <el-aside :width="leftAsideWidth"
-                  class="home-aside">
+          class="home-aside">
           <fr-menu :data="menuData"
-                   :menu-click="handleMenuClick"></fr-menu>
+            :menu-click="handleMenuClick"
+            style="overflow: auto; height: 100%;"></fr-menu>
         </el-aside>
         <el-main class="padding__empty">
           <fr-router-view-tabs></fr-router-view-tabs>
@@ -34,45 +36,13 @@
   </el-container>
 </template>
 
-<style>
-.fr-full {
-  width: 100%;
-  height: 100%;
-}
-
-.home-conatiner {
-}
-
-.home-header {
-  background-color: #6699cc;
-}
-
-.home-header-title {
-  padding: 10px;
-  font-weight: 600;
-  font-family: "微软雅黑";
-  color: #333333;
-}
-
-.home-header-title__logo {
-  width: 80px;
-  height: 40px;
-  vertical-align: middle;
-  display: inline-block;
-}
-
-.home-aside {
-  background: #99ccff;
-}
-</style>
-
 <script>
-import FrMenu from '../components/menu'
-import FrRouterViewTabs from '../components/router-view-tabs'
+import FrMenu from '../components/menu';
+import FrRouterViewTabs from '../components/router-view-tabs';
 
-import MenuData from '../assets/data/menus'
+import MenuData from '../assets/data/menus';
 
-import systemRoutes from '../assets/data/routes_system'
+import systemRoutes from '../assets/data/routes_system';
 
 export default {
   name: 'home',
@@ -80,28 +50,28 @@ export default {
     FrMenu,
     FrRouterViewTabs
   },
-  data () {
+  data() {
     return {
       leftAsideWidth: '300px',
       rightAsideWidth: '300px',
       menuData: MenuData
-    }
+    };
   },
   methods: {
-    handleMenuClick (menu) {
+    handleMenuClick(menu) {
       this.$router.push({
         target: menu.target,
         title: menu.label,
         path: menu.url || menu.path,
         query: menu.query,
         params: menu.params
-      })
+      });
     },
 
-    loadRoute () {
+    loadRoute() {
       setTimeout(() => {
-        this.$store.commit('Route/setRoutes', systemRoutes)
-      }, 300)
+        this.$store.commit('Route/setRoutes', systemRoutes);
+      }, 300);
     }
   }
 }
